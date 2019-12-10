da2b2ae3ff591c26d27ef66dcfbfacf13592acb3
김윤수 <younsookim@tmon.co.kr>
2019-12-10 17:32:48 +0900

    2, 타락한 객체

--- tdd-dollar/src/main/java/study/tdd/demo/tdddollar/Dollar.java
+++ tdd-dollar/src/main/java/study/tdd/demo/tdddollar/Dollar.java
@@ -3,11 +3,13 @@ package study.tdd.demo.tdddollar;
 public class Dollar {
 	int amount;
 
+
+	public Dollar times(int multiplier) {
+		return new Dollar(this.amount * multiplier);
+	}
+
 	public Dollar(int amount) {
 		this.amount = amount;
 	}
 
-	public void times(int multiplier) {
-		this.amount *= multiplier;
-	}
 }
