ab5e105ef8732eb797e2fdbb83493161d2c0a37f
김윤수 <younsookim@tmon.co.kr>
2019-12-10 17:02:13 +0900

    1, 다중 통화를 지원하는 Money객체

--- tdd-dollar/src/main/java/study/tdd/demo/tdddollar/Dollar.java
+++ tdd-dollar/src/main/java/study/tdd/demo/tdddollar/Dollar.java
@@ -3,11 +3,11 @@ package study.tdd.demo.tdddollar;
 public class Dollar {
 	int amount;
 
-	public int times(int multiplier) {
-		return this.amount * multiplier;
-	}
-
 	public Dollar(int amount) {
 		this.amount = amount;
+	}
+
+	public void times(int multiplier) {
+		this.amount *= multiplier;
 	}
 }
